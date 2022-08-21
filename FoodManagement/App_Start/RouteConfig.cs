6cd34309cf9ad1784e29fe3bb247b9176da27123
AashifAmeer <aashifameer6833@gmail.com>
2022-08-21 17:33:26 +0530

    Created Views, registration form and Login form

--- FoodManagement/App_Start/RouteConfig.cs
+++ FoodManagement/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FoodManagement
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Front", id = UrlParameter.Optional }
             );
         }
     }
--- FoodManagement/Controllers/HomeController.cs
+++ FoodManagement/Controllers/HomeController.cs
@@ -11,9 +11,75 @@ namespace FoodManagement.Controllers
     {
         // GET: Home
         FoodManagementEntities food = new FoodManagementEntities();
-        public ActionResult Index()
+        public ActionResult Front()
         {
             return View();
+        }
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(USER_REGISTRATION user)
+        {
+            if (ModelState.IsValid)
+            {
+                food.USER_REGISTRATION.Add(user);
+                food.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return View(user);
+            }
+        }
+        [Authorize]
+        public ActionResult Content()
+        {
+
+            return View();
+        }
+        
+        //[ValidateAntiForgeryToken]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        public ActionResult Authenticate()
+        {
+            if (ModelState.IsValid)
+            {
+                var data = food.USER_REGISTRATION.ToList();
+                int count = 0;
+
+                foreach (USER_REGISTRATION item in data)
+                {
+
+                    if (Request.Form["email"].Equals(item.EMAIL) && Request.Form["password"].Equals(item.PASSWORD))
+                    {
+
+                        count++;
+                    }
+
+                }
+                if (count > 0)
+                {
+                    return View("Content");
+                }
+                else
+                {
+                    return View("Login");
+                }
+
+            }
+            else
+            {
+                return View("Login");
+            }
         }
     }
 }
